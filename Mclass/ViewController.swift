36ba479ce7c01c94bc1a07d68f7bb498749270f7
Neal Parikh <nealrp@umich.edu>
2015-05-22 15:35:41 -0400

    Added Basic CamView Storyboard

--- Mclass/ViewController.swift
+++ Mclass/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func SwitchToCamView(sender: AnyObject) {
+       
+
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ChangeScreens(sender: AnyObject) {
+        
+        let viewController:UIViewController = UIStoryboard(name: "CamView", bundle: nil).instantiateViewControllerWithIdentifier("CamViewSB") as! UIViewController
+        // .instantiatViewControllerWithIdentifier() returns AnyObject! this must be downcast to utilize it
+        
+        self.presentViewController(viewController, animated: false, completion: nil)
+        
+    }
 
 }
 
