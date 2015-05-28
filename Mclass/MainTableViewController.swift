75d595b59404a18257814faa32116a597db747b6
samirish17 <samirish@umich.edu>
2015-05-28 13:58:31 -0400

    Added main page modes with highlight functionality

--- Mclass/MainTableViewController.swift
+++ Mclass/MainTableViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
+    
+    var modes: [String] = ["Laptop", "Document Camera", "Apple TV", "Hide Screen", "Projector On/Off"]
+    var selectedMode:String? = nil
+    var selectedModeIndex:Int? = nil
 
     @IBOutlet var sourceTable: UITableView!
     
@@ -33,14 +37,51 @@ class MainTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return modes.count
     }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ModeCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = modes[indexPath.row]
+        
+        // Configure the cell...
+        /*if indexPath.row == selectedModeIndex {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }*/
+        cell.textLabel?.font = UIFont.systemFontOfSize(30.0)
+        cell.textLabel?.textColor = UIColor(red:14/255, green:122/255, blue:254/255, alpha:1.0)
+        return cell
+    }
+    
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)
+        
+        //Other row is selected - need to deselect it
+        if let index = selectedModeIndex {
+            let cell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: index, inSection: 0))
+            cell?.backgroundColor = UIColor.clearColor()
+        }
+        
+        selectedModeIndex = indexPath.row
+        selectedMode = modes[indexPath.row]
+        
+        //update the checkmark for the current row
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        //cell?.accessoryType = .Checkmark
+        
+        cell?.backgroundColor = UIColor(red:14/255, green:50/255, blue:100/255, alpha:0.4)
+    }
+    
+    
 
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
