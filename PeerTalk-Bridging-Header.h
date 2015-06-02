c6a9f2dc1a5e9820d7b41a46b4f88f6f139592cc
samirish17 <samirish@umich.edu>
2015-06-02 13:32:19 -0400

    New branch with cocoa pod and PeerTalk integration

---
+++ PeerTalk-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PeerTalk-Bridging-Header.h
+//  Mclass
+//
+//  Created by Samuel Irish on 6/2/15.
+//  Copyright (c) 2015 CAENiOS. All rights reserved.
+//
+
+#import "PTChannel.h"
+#import "PTProtocol.h"
+#import "PTUSBHub.h"
+
+
