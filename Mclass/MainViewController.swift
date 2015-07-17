7a6b7192fd48a6106f85419fc93f405bebc190dd
Mike Dobrowolski <mdobro@umich.edu>
2015-07-17 13:01:29 -0400

    bug fixes for proj error handling

--- Mclass/MainViewController.swift
+++ Mclass/MainViewController.swift
@@ -105,7 +105,10 @@ class MainViewController: UIViewController, PTChannelDelegate, SettingsViewContr
             self.dismissViewControllerAnimated(true, completion: nil)
         }
         else {
-            self.dismissViewControllerAnimated(false, completion: nil)
+            self.dismissViewControllerAnimated(false, completion: { _ in
+                let alert = UIAlertController(title: "PROJECTOR \(action)ERROR!", message: "For help please contact CAEN:\n\nPhone: (734)-764-CAEN\nEmail: caen@umich.edu", preferredStyle: UIAlertControllerStyle.Alert)
+                self.presentViewController(alert, animated: true, completion: nil)
+            })
             let alert = UIAlertController(title: "PROJECTOR \(action)ERROR!", message: "For help please contact CAEN:\n\nPhone: (734)-764-CAEN\nEmail: caen@umich.edu", preferredStyle: UIAlertControllerStyle.Alert)
             self.presentViewController(alert, animated: true, completion: nil)
         }
